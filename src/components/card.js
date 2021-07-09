5541e14993b6cea234d74c529ee85eff45df26d7
JacobGriffithElliott <j.o.gelliott@gmail.com>
2021-07-09 11:07:42 -0700

    MVP

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,56 @@
+import axios from "axios"
+
 const Card = (article) => {
-  // TASK 5
+  // Body
+  const cardBody = document.createElement('div')
+  cardBody.classList.add('card')
+  // Headline
+  const headlineDiv = document.createElement('div')
+  headlineDiv.classList.add('headline')
+  headlineDiv.textContent = article.headline
+  cardBody.appendChild(headlineDiv)
+  // Author
+  const authorDiv = document.createElement('div')
+  authorDiv.classList.add('author')
+  cardBody.appendChild(authorDiv)
+  // Image container
+  const imgCon = document.createElement('div')
+  imgCon.classList.add('img-container')
+  authorDiv.appendChild(imgCon)
+  // Image
+  const img = document.createElement('img')
+  img.src = article.authorPhoto
+  imgCon.appendChild(img)
+  // Author Name
+  const authName = document.createElement('span')
+  authName.textContent = article.authorName
+  authorDiv.appendChild(authName)
+  // Event listener
+  cardBody.addEventListener('click', () =>{ console.log(article.headline)
+})
+  return cardBody
+}
+
+const cardAppender = (selector) => {
+  axios.get('http://localhost:5000/api/topics')
+  .then(res => {
+    res.data.topics.forEach(topic => {
+      axios.get('http://localhost:5000/api/articles')
+      .then(res => {
+        let target = document.querySelector(selector)
+        if(topic === 'node.js'){
+          topic = "node"
+        }
+        let cardData = (res.data.articles[topic])
+        for(let i=0;i<cardData.length;i++){
+          target.append(Card(cardData[i]))
+        }
+      })
+    })
+  })
+}
+
+// TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
   // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
@@ -17,9 +68,7 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-}
 
-const cardAppender = (selector) => {
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
@@ -28,6 +77,4 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
-}
-
 export { Card, cardAppender }
